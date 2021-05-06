d4cbee2ebd0a5a91aaa49b24004242d7c9242b1f
Shivansh Goel <shivanshgoel007@gmail.com>
2021-05-06 19:57:59 +0530

    Fixed activity interaction when photo fragment is active

--- app/src/main/java/com/goel/peerlocator/fragments/ImageViewFragment.kt
+++ app/src/main/java/com/goel/peerlocator/fragments/ImageViewFragment.kt
@@ -1,6 +1,7 @@
 package com.goel.peerlocator.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -43,6 +44,10 @@ class ImageViewFragment : Fragment() {
         binding?.imageEditButton?.setOnClickListener {
             if (editable)
                 editProfilePhoto()
+        }
+
+        binding?.root?.setOnClickListener {
+            Log.d("FRAME: ", "Photo frame clicked")
         }
 
         return binding?.root
