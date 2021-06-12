69882cbb61932213fd357a7f837d4155f98a3e1f
Shivansh Goel <shivanshgoel007@gmail.com>
2021-06-12 12:37:40 +0530

    Implemented send invitation on UserInfoActivity

--- app/src/main/java/com/goel/peerlocator/activities/UserInfoActivity.kt
+++ app/src/main/java/com/goel/peerlocator/activities/UserInfoActivity.kt
@@ -2,10 +2,14 @@ package com.goel.peerlocator.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.goel.peerlocator.R
 import com.goel.peerlocator.databinding.ActivityUserInfoBinding
+import com.goel.peerlocator.dialogs.InvitationLoadingDialog
+import com.goel.peerlocator.listeners.AddFriendListener
 import com.goel.peerlocator.models.UnknownUserModel
+import com.goel.peerlocator.repositories.InvitesRepository
 import com.squareup.picasso.Picasso
 
 class UserInfoActivity : AppCompatActivity() {
@@ -24,6 +28,8 @@ class UserInfoActivity : AppCompatActivity() {
 
         createToolBar()
         setData()
+
+        setClickListeners()
     }
 
     private fun createToolBar() {
@@ -39,5 +45,32 @@ class UserInfoActivity : AppCompatActivity() {
         val name = model.name
         binding.infoToolbar.profileName.text = name
         Picasso.with(this).load(photoUrl).placeholder(R.drawable.ic_placeholder_user_big).into(binding.profileImageHolder)
+    }
+
+    private fun setClickListeners () {
+        binding.sendInvitationButton.setOnClickListener {
+            sendInvitation()
+        }
+    }
+
+    private fun sendInvitation() {
+        val loading = InvitationLoadingDialog("Sending Invitation")
+        loading.show(supportFragmentManager, "sending invitation")
+        InvitesRepository.instance.sendInvitation(model, object : AddFriendListener {
+            override fun onInvitationSent(model: UnknownUserModel) {
+                loading.dismiss()
+                Toast.makeText(this@UserInfoActivity, R.string.invitation_sent, Toast.LENGTH_SHORT)
+                    .show()
+                finish()
+            }
+
+            override fun onInvitationUnsent(model: UnknownUserModel) {}
+
+            override fun onError() {
+                loading.dismiss()
+                Toast.makeText(this@UserInfoActivity, R.string.error_message, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        })
     }
 }
